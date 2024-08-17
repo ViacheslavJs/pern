af720f0153dd184beb26235d2f36a9abebfed502
ViacheslavJS <cherniienko7@gmail.com>
2024-08-17 14:07:21 +0300

    test connect

--- server/index.js
+++ server/index.js
@@ -19,12 +19,12 @@ if (USE_DATABASE) {
 
 
 const pool = new Pool({
-  //user: process.env.DB_USER,
-  //host: process.env.DB_HOST,
-  //database: process.env.DB_NAME,
-  //password: process.env.DB_PASSWORD,
-  //port: process.env.DB_PORT,
-  connectionString: process.env.INTERNAL_DB_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  //connectionString: process.env.INTERNAL_DB_URL,
 });
 
 const TABLE_NAME = 'products';
